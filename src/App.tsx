071f1f43e21dd2f0ead1a8c929a542f5f2edafa8
Stan <guseletovS@gmail.com>
2021-09-17 14:18:04 +0100

    latest changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,63 @@
+
+
+// interface Joke {
+//   id: number;
+//   type: string;
+//   setup: string;
+//   punchline: string;
+// }
+
 import { useState } from "react";
 
-interface Joke {
-  id: number;
-  type: string;
-  setup: string;
-  punchline: string;
+function App() {
+
+  const [dog, setDog] = useState<Doggo>();
+  
+  interface Doggo {
+      message: string,
+      status: string
+      }
+  
+  const getMeADog = async () => {
+      const response = await fetch(
+        "https://dog.ceo/api/breeds/image/random"
+      );
+      const jsonBody: Doggo = await response.json();
+      setDog(jsonBody)
+    }
+  
+    if (dog){
+      return(
+          <div>
+            <h1>Doggo Pic App</h1>
+            <button onClick = {getMeADog}> Click me to get a doggo</button>
+            <img src = {dog.message} alt = "doggo pic" />
+          </div>  
+      )
+  }
+  else {
+      return (
+        <div>
+        <h1>Doggo Pic App</h1>
+        <button onClick = {getMeADog}> Click me to get a doggo</button>
+        <p>
+          Click the button to trigger a <code>fetch</code> that gets a random
+          doggo pic from an API!
+        </p>
+      </div>  
+      )
+  }
 }
 
-function App() {
-  const [joke, setJoke] = useState<Joke>();
+  // const [joke, setJoke] = useState<Joke>();
 
-  const handleGetJoke = async () => {
-    const response = await fetch(
-      "https://official-joke-api.appspot.com/jokes/general/random"
-    );
-    const jsonBody: Joke[] = await response.json();
-    setJoke(jsonBody[0]);
-  };
+  // const handleGetJoke = async () => {
+  //   const response = await fetch(
+  //     "https://official-joke-api.appspot.com/jokes/general/random"
+  //   );
+  //   const jsonBody: Joke[] = await response.json();
+  //   setJoke(jsonBody[0]);
+  // };
 
   // const handleGetJoke = () => {
   //   fetch("https://official-joke-api.appspot.com/jokes/general/random")
@@ -24,30 +65,30 @@ function App() {
   //     .then((jsonBody: Joke[]) => setJoke(jsonBody[0]));
   // };
 
-  if (joke) {
-    return (
-      <div>
-        <h1>Joke app</h1>
-        <details>
-          <summary>{joke.setup}</summary>
-          <p>{joke.punchline}</p>
-        </details>
-        <hr />
-        <button onClick={handleGetJoke}>Get another joke</button>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <h1>Joke app</h1>
-        <p>
-          Click the button to trigger a <code>fetch</code> that gets a random
-          joke from an API!
-        </p>
-        <button onClick={handleGetJoke}>Get joke</button>
-      </div>
-    );
-  }
-}
+//   if (joke) {
+//     return (
+//       <div>
+//         <h1>Joke app</h1>
+//         <details>
+//           <summary>{joke.setup}</summary>
+//           <p>{joke.punchline}</p>
+//         </details>
+//         <hr />
+//         <button onClick={handleGetJoke}>Get another joke</button>
+//       </div>
+//     );
+//   } else {
+//     return (
+//       <div>
+//         <h1>Joke app</h1>
+//         <p>
+//           Click the button to trigger a <code>fetch</code> that gets a random
+//           joke from an API!
+//         </p>
+//         <button onClick={handleGetJoke}>Get joke</button>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
